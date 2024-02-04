65f6a693cb76f550ec764a1b09bd5d5aebbe0efc
Ihor Rymaruk <rymaruk@gmail.com>
2024-02-03 18:14:22 -0600

    query numer list

--- one/background.js
+++ one/background.js
@@ -1,6 +1,13 @@
 const startUrl = 'https://e.land.gov.ua/back/parcel_registration';
 const searchUrl = 'https://e.land.gov.ua/back/cadaster/';
 
+function searchPage() {  
+  const allList = document.querySelectorAll("#parcel_registration_parcelsForUnion  label");
+  console.info('All list length =' + allList.length);
+  const cadNums = [...allList].map(n => n.innerText.replace(/ перетин.*/, ""));
+  console.info("CadNums = " + cadNums);
+}
+
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(startUrl)) {
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
@@ -10,6 +17,10 @@ chrome.action.onClicked.addListener(async (tab) => {
       text: nextState
     });
 
-    console.info("Here");
+    console.info('Here');
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: searchPage
+    });
   }
 });
